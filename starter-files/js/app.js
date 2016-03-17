da41a9e6a8f18d9be6109265b77486b86b8a975b
travis <t3patterson@gmail.com>
2016-03-17 16:33:26 -0500

    adding meta tags

---
+++ starter-files/js/app.js
@@ -0,0 +1,16 @@
+// 01 - Configure Router
+//---------------------------------
+
+// (0) Make sure jQuery, _, and BB are wired UP!
+
+// (1) extend Backbone.Router
+
+// (2) create router instance with `new` keyword
+
+// (3) set routes + route-callback functions
+
+// (4) create route-callback functions for our 2 routes
+
+// (5) tell the router to take over browser-history/hash-routes with Backbone.history.start()
+//      in the Router's `initialize` function
+
